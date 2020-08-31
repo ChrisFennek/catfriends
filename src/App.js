531349bc168e854b05ae16b6edacae9da205f526
ChrisFennek <christian.gebhardt@online.de>
2020-08-31 21:54:43 +0200

    New sorting

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
 import { cats } from "./cats";
 
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      cats: cats,
+      searchfield: ""
+    }
+  }
 
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value });
+  }
 
-const App = () => {
-  return (
-    <div>
-      <h1>CatFriends</h1>
-      <SearchBox />
-      <CardList cats={cats} />
-    </div>
-  );
+  render() {
+    const filteredCats = this.state.cats.filter(cats => {
+      return cats.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    })
+    return (
+      <div className="tc">
+        <h1>CatFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList cats={filteredCats} />
+      </div>
+    );
+  }
 }
+
 
 export default App;
