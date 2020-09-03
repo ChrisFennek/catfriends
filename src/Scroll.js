101d80b95a177f588dcff2caa0d63499e712a46e
ChrisFennek <christian.gebhardt@online.de>
2020-09-03 16:12:31 +0200

    MiniChange

--- src/Scroll.js
+++ src/Scroll.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Scroll(props) {
   return (
     <div style={{
-      overflowY: "scroll", border: "1px solid black", height: "500px"
+      overflowY: "scroll", border: "1px solid black", height: "800px"
     }}>
       { props.children}
     </div >
