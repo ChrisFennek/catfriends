14a1c3eebbcb8e7a582279088b8efd204daccc6f
ChrisFennek <christian.gebhardt@online.de>
2020-09-07 21:54:19 +0200

    More cards

--- src/cats.js
+++ src/cats.js
@@ -1,25 +1,37 @@
 export const cats = [{
-    id: 1,
-    name: "Pixie",
-    age: "6",
-    food: "Tuna"
-  },
-  {
-    id: 2,
-    name: "Miau",
-    age: "4",
-    food: "Shrimps"
-  },
-  {
-    id: 3,
-    name: "Pepper",
-    age: "1",
-    food: "Chicken"
-  },
-  {
-    id: 4,
-    name: "Jack",
-    age: "5",
-    food: "Duck"
-  }
+  id: 1,
+  name: "Pixie",
+  age: "6",
+  food: "Tuna"
+},
+{
+  id: 2,
+  name: "Miau",
+  age: "4",
+  food: "Shrimps"
+},
+{
+  id: 3,
+  name: "Pepper",
+  age: "1",
+  food: "Chicken"
+},
+{
+  id: 4,
+  name: "Bill",
+  age: "7",
+  food: "Ham"
+},
+{
+  id: 5,
+  name: "Toga",
+  age: "3",
+  food: "Rice"
+},
+{
+  id: 6,
+  name: "Leona",
+  age: "9",
+  food: "Lobster"
+}
 ];
