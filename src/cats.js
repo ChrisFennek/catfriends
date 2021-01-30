51d25f82d1edb9a970058e0c2989e7f33a059c3e
ChrisFennek <christian.gebhardt@online.de>
2021-01-30 21:14:34 +0100

    update

--- src/cats.js
+++ src/cats.js
@@ -1,37 +1,43 @@
 export const cats = [{
-  id: 1,
-  name: "Pixie",
-  age: "6",
-  food: "Tuna"
-},
-{
-  id: 2,
-  name: "Miau",
-  age: "4",
-  food: "Shrimps"
-},
-{
-  id: 3,
-  name: "Pepper",
-  age: "1",
-  food: "Chicken"
-},
-{
-  id: 4,
-  name: "Bill",
-  age: "7",
-  food: "Ham"
-},
-{
-  id: 5,
-  name: "Toga",
-  age: "3",
-  food: "Rice"
-},
-{
-  id: 6,
-  name: "Leona",
-  age: "9",
-  food: "Lobster"
-}
+    id: 1,
+    name: "Pixie",
+    age: "6",
+    food: "Tuna"
+  },
+  {
+    id: 2,
+    name: "Miau",
+    age: "4",
+    food: "Shrimps"
+  },
+  {
+    id: 3,
+    name: "Pepper",
+    age: "1",
+    food: "Chicken"
+  },
+  {
+    id: 4,
+    name: "Bill",
+    age: "7",
+    food: "Ham"
+  },
+  {
+    id: 5,
+    name: "Toga",
+    age: "3",
+    food: "Rice"
+  },
+  {
+    id: 6,
+    name: "Leona",
+    age: "9",
+    food: "Lobster"
+  },
+  {
+    id: 7,
+    name: "Luna",
+    age: "3",
+    food: "Sausage"
+  }
 ];
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,41 +1,56 @@
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
-import { cats } from "../cats";
+import {
+  cats
+} from "../cats";
 import Scroll from "../components/Scroll";
 import "./App.css";
 
 class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      cats: [],
-      searchfield: ""
-    }
+
+  this.state = {
+    cats: [],
+    searchfield: ""
+
   }
 
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then(response => response.json())
-      .then(users => this.setState({ cats: cats }));
+      .then(users => this.setState({
+        cats: cats
+      }));
   }
 
   onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value });
+    this.setState({
+      searchfield: event.target.value
+    });
   }
 
   render() {
     const filteredCats = this.state.cats.filter(cat => {
       return cat.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
     })
-    return (
-      <div className="tc">
-        <h1 className="f1">CatFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <Scroll>
-          <CardList cats={filteredCats} />
-        </Scroll>
-      </div>
+    return ( <
+      div className = "tc" >
+      <
+      h1 className = "f1" > CatFriends < /h1> <
+      SearchBox searchChange = {
+        this.onSearchChange
+      }
+      /> <
+      Scroll >
+      <
+      CardList cats = {
+        filteredCats
+      }
+      /> <
+      /Scroll> <
+      /div>
     );
   }
 }
